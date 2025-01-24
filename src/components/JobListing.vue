eb21255802c098957f55023a6d77c9e9e311fc31
mohdaizat34 <52664189+mohdaizat34@users.noreply.github.com>
2025-01-24 15:47:12 +0800

    added an icon using primeicon

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,10 +1,23 @@
 <script setup>
 import { defineProps, ref, computed } from "vue";
 
-defineProps({
+const props = defineProps({
   job: {
     type: Object,
   },
+});
+
+const showFullDescription = ref(false);
+const toggleDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+};
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description;
+  if (!showFullDescription.value) {
+    description = description.substring(0, 90) + "...";
+  }
+  return description;
 });
 </script>
 
@@ -17,7 +30,16 @@ defineProps({
       </div>
 
       <div class="mb-5">
-        {{ job.description }}
+        <div>
+          {{ truncatedDescription }}
+        </div>
+
+        <button
+          @click="toggleDescription"
+          class="text-green-500 hover:text-green-600 mb-5"
+        >
+          {{ showFullDescription ? "Show Less" : "More" }}
+        </button>
       </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
@@ -26,7 +48,7 @@ defineProps({
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
         <a
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -20,7 +20,7 @@ const jobs = ref(jobData);
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gaps-6">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
           v-for="job in jobs.slice(0, limit || jobs.length)"
           :key="job.id"
