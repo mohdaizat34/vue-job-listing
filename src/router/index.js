230a16a2e1ecab084b83db79d74ad100437cdea8
mohdaizat34 <52664189+mohdaizat34@users.noreply.github.com>
2025-01-26 14:49:22 +0800

    add some router href

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory } from  'vue-router'
 import  HomeView  from  '@/views/HomeView.vue'
+import JobsView from '@/views/JobsView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,12 @@ const router = createRouter({
             path: '/',
             name: 'HomeView',
             component: HomeView
+        },
+
+        {
+            path: '/jobs',
+            name: 'JobsView',
+            component: JobsView
         }
     ]
 });
