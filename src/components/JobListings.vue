e3966e574019c6db9320b29a4355959361cd7fcf
mohdaizat34 <52664189+mohdaizat34@users.noreply.github.com>
2025-01-30 18:08:15 +0800

    add get to fetch the api from json and add loading progress bar

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,9 @@
 <script setup>
-import jobData from "@/jobs.json";
 import JobListing from "./JobListing.vue";
 import { RouterLink } from "vue-router";
-import { ref, defineProps } from "vue";
+import { ref, defineProps, onMounted } from "vue";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import axios from "axios";
 
 defineProps({
   limit: Number,
@@ -12,7 +13,21 @@ defineProps({
   },
 });
 
-const jobs = ref(jobData);
+const isLoading = ref(true);
+const jobs = ref([]);
+
+onMounted(async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // to stimulate loading
+  try {
+    const response = await axios.get("http://localhost:8000/jobs");
+    console.log("API Response:", response.data); // Log the response data
+    jobs.value = response.data;
+  } catch (error) {
+    console.error("API Error:", error); // Log any errors
+  } finally {
+    isLoading.value = false;
+  }
+});
 </script>
 
 <template>
@@ -21,7 +36,12 @@ const jobs = ref(jobData);
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <!--Show loading spinner while loading is true-->
+      <div v-if="isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+      <!--Show job listings when loading is done-->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
           v-for="job in jobs.slice(0, limit || jobs.length)"
           :key="job.id"
