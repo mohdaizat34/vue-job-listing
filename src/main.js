ca7ace22c977ff177b8c64b0748bfcf6ba2bf572
mohdaizat34 <52664189+mohdaizat34@users.noreply.github.com>
2025-02-04 11:58:30 +0800

    fix  toast notification

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './assets/main.css'
 import "primeicons/primeicons.css";
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
@@ -7,6 +9,7 @@ import router from './router';
 const app = createApp(App)
 
 app.use(router) 
+app.use(Toast)
 app.mount('#app')
 
 
