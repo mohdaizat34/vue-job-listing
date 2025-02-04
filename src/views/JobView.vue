3854c6473b05db60db8e88772cd1fd56a405ff75
mohdaizat34 <52664189+mohdaizat34@users.noreply.github.com>
2025-02-04 12:51:26 +0800

    add edit function

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -109,10 +109,10 @@ onMounted(async () => {
           <!-- Manage -->
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-            <a
-              href="add-job.html"
+            <RouterLink
+              :to="`/jobs/edit/${state.job.id}`"
               class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-              >Edit Job</a
+              >Edit Job</RouterLink
             >
             <button
               @click="deleteJob"
